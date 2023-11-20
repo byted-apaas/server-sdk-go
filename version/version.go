1ae8d4757dad24c32bbbcf6b4697ecebd4b0b0a5
zhouwexin <zhouwexin@bytedance.com>
2023-11-20 14:13:10 +0800

    auto sync

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 	cVersion "github.com/byted-apaas/server-common-go/version"
 )
 
-const Version = "0.0.0-beta.1"
+const Version = "v0.0.26-beta.1"
 
 const SDKName = "byted-apaas/server-sdk-go"
 
